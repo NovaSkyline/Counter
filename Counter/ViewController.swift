ff6f773475a3b664483d4babc6b53d42a24fdb74
Skyline <r_kamalov@mail.ru>
2024-08-18 00:01:12 +0200

    Добавлена кнопка и лейбл счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var countLabel: UILabel!
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = "Значение счётчика: 0"
     }
-
-
-}
-
+    
+    @IBAction func buttonDidIncrease(_ sender: Any) {
+            counter += 1
+            countLabel.text = "Значение счётчика: \(counter)"
+        }
+        
+    }
+   
